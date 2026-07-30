4e7cff9828c82fae5e8ce95a75428b1e15da62e3
Natalia Davydova <52240221+nat-davydova@users.noreply.github.com>
2026-07-30 18:53:38 +0300

    feat: add router with routes

--- course-project/src/app/router/router.tsx
+++ course-project/src/app/router/router.tsx
@@ -3,11 +3,29 @@ import { createBrowserRouter } from 'react-router'
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello FSD!!!</div>,
+    element: <div>Home</div>,
   },
 
   {
-    path: '/hehe',
-    element: <div>Hehehehe?</div>,
+    path: '/community',
+    element: <div>Community</div>,
+  },
+
+  {
+    path: '/:alias',
+    element: <div>Category</div>,
+  },
+
+  {
+    path: '/article',
+    children: [{ path: ':alias', element: <div>Article</div> }],
+  },
+
+  {
+    path: '/profile',
+    children: [
+      { index: true, element: <div>Profile</div> },
+      { path: 'edit', element: <div>Edit Profile</div> },
+    ],
   },
 ])
