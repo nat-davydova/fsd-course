e8d7a2db67125c88d1147fa1105e4335b9fb7039
Natalia Davydova <52240221+nat-davydova@users.noreply.github.com>
2026-07-30 18:08:16 +0300

    refactor: apply app layer (in progress)

--- course-project/src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <div>Ready to work</div>
-  )
-}
-
-export default App
---
+++ course-project/src/app/routes/routes.tsx
@@ -0,0 +1,13 @@
+import {createBrowserRouter} from "react-router";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hello FSD!!!</div>
+    },
+
+    {
+        path: '/hehe',
+        element: <div>Hehehehe?</div>
+    }
+])
